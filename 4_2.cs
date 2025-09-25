725b3f054cca65d1e4196891981c0ccecc5573d5
nikitalogvin2885-svg <nikitalogvin2885@gmail.com>
2025-09-25 21:01:06 +0300

    Add files via upload

---
+++ 4_2.cs
@@ -0,0 +1,24 @@
+﻿//16
+using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины трех сторон треугольника:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            bool isTrianglePossible = IsTrianglePossible(a, b, c);
+            Console.WriteLine(isTrianglePossible ? "Треугольник можно построить." : "Треугольник нельзя построить.");
+        }
+
+        static bool IsTrianglePossible(int a, int b, int c)
+        {
+            return a + b > c && a + c > b && b + c > a;
+        }
+    }
+}
